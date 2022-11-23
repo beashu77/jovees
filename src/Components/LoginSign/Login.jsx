f48df919322852d3444d4c34dc959d5eefa137fd
Ashwini Bhoskar <ashwinibhoskar5111@gmail.com>
2022-11-24 02:44:08 +0530

    Login and Signup form completed

--- src/Components/LoginSign/Login.jsx
+++ src/Components/LoginSign/Login.jsx
@@ -1,9 +1,92 @@
-import React from 'react'
+import {
+  Box,
+  Button,
+  Flex,
+  FormLabel,
+  Input,
+  Text,
+} from "@chakra-ui/react";
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 const Login = () => {
-  return (
-    <div>Login</div>
-  )
-}
+  const navigate = useNavigate()
+  const [form,setForm] =useState({});
+  const handleChange=(e)=>{
+      const {name,type,value,checked} = e.target;
+      if(type==='Checkbox')
+      {
+        setForm({
+          ...form,
+          [name]:checked
+        })
+      }
+      else{
+        setForm({
+          ...form,
+          [name]:value
+        })
+      }
+ 
+  }
 
-export default Login
+  const handleSubmit =(e)=>{
+    e.preventDefault();
+    console.log(form);
+
+  }
+  return (
+    <Flex justifyContent="center" alignItems="center" height="80vh">
+      <form>
+        <Box padding="2rem" maxW="1200px">
+          <Text
+            padding="0.2rem 0rem"
+            textAlign="left"
+            fontSize="1xl"
+            fontWeight="600"
+          >
+            REGISTERED CUSTOMERS
+          </Text>
+          <Text textAlign="left" color="lightgray" fontSize="sm">
+            {" "}
+            <em>
+            If you have an account, sign in with your email address.
+            </em>
+          </Text>
+          <Flex justifyContent="space-evenly" gap="15px" flexDirection="column">
+            <FormLabel>Email address</FormLabel>
+            <Input w="350px" borderRadius="none" type="email"      name="email"
+              value={form.email||''}   onChange={handleChange}/>
+
+            <FormLabel>Password</FormLabel>
+            <Input w="350px" borderRadius="none" type="password"      name="password"
+              value={form.password||''}   onChange={handleChange}/>
+          </Flex>
+        </Box>
+        <Flex justifyContent='space-evenly'>
+          <Button
+            w="40%"
+            borderRadius="15px"
+            backgroundColor="white"
+            border='2px solid'
+            // onClick={navigate("/")}
+          >
+            <Link to="/">Back</Link>
+          </Button>
+          <Button
+            w="40%"
+            borderRadius="0px"
+            backgroundColor="black"
+            color="white"
+            _hover={{ backgroundColor: "#10693c" }}
+            onClick={handleSubmit}
+          >
+           Login
+          </Button>
+        </Flex>
+      </form>
+    </Flex>
+  );
+};
+
+export default Login;
--- src/Components/LoginSign/Signup.jsx
+++ src/Components/LoginSign/Signup.jsx
@@ -1,12 +1,125 @@
-import React from 'react';
-import style from "./SignStyle.module.css"
+import {
+  Box,
+  Button,
+  Checkbox,
+  Flex,
+  FormLabel,
+  Input,
+  Text,
+} from "@chakra-ui/react";
+import React from "react";
+import { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
 
 const Signup = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const navigate = useNavigate()
+  const [form,setForm] =useState({});
+  const handleChange=(e)=>{
+      const {name,type,value,checked} = e.target;
+      if(type==='Checkbox')
+      {
+        setForm({
+          ...form,
+          [name]:checked
+        })
+      }
+      else{
+        setForm({
+          ...form,
+          [name]:value
+        })
+      }
+ 
+  }
 
-export default Signup
+  const handleSubmit =(e)=>{
+    e.preventDefault();
+    console.log(form);
+
+  }
+  return (
+    <Flex justifyContent="center" alignItems='center' height='90vh'>
+      <form>
+        <Box  padding="2rem" maxW="1200px">
+          <Text
+            paddingBottom="1rem"
+            textAlign="left"
+            fontSize="1xl"
+            fontWeight="600"
+          >
+            PERSONAL INFORMATION
+          </Text>
+          <Flex justifyContent="space-evenly" gap="15px">
+            <Input
+              w="350px"
+              borderRadius="none"
+              type="text"
+              placeholder="First Name"
+              onChange={handleChange}
+              name="FirstName"
+              value={form.FirstName||''}
+            />
+            <Input
+              w="350px"
+              borderRadius="none"
+              type="text"
+              placeholder="Last Name"
+              onChange={handleChange}
+              name="LastName"
+              value={form.LastName||''}
+            />
+          </Flex>
+          <Flex padding="1.2rem 0rem">
+            <Checkbox marginRight='15px'/>
+            <Text fontSize="sm">Sign Up for Newsletter</Text>
+          </Flex>
+          <Flex>
+            <Checkbox marginRight='15px'/>
+            <Text fontSize="sm">Allow remote shopping assistance</Text>
+          </Flex>
+
+          <Text
+            padding="2rem 0rem"
+            textAlign="left"
+            fontSize="1xl"
+            fontWeight="600"
+          >
+            SIGN-IN INFORMATION
+          </Text>
+          <Flex justifyContent="space-evenly" gap="15px" flexDirection="column">
+            <FormLabel>Email address</FormLabel>
+            <Input w="350px" borderRadius="none" type="email"      name="email"
+              value={form.email||''}   onChange={handleChange}/>
+
+            <FormLabel>Password</FormLabel>
+            <Input w="350px" borderRadius="none" type="password"      name="password"
+              value={form.password||''}   onChange={handleChange}/>
+          </Flex>
+        </Box>
+        <Flex justifyContent='space-evenly'>
+          <Button
+            w="40%"
+            borderRadius="15px"
+            backgroundColor="white"
+            border='2px solid'
+            // onClick={navigate("/")}
+          >
+            <Link to="/">Back</Link>
+          </Button>
+          <Button
+            w="40%"
+            borderRadius="0px"
+            backgroundColor="black"
+            color="white"
+            _hover={{ backgroundColor: "#10693c" }}
+            onClick={handleSubmit}
+          >
+            Create an Account
+          </Button>
+        </Flex>
+      </form>
+    </Flex>
+  );
+};
+
+export default Signup;
